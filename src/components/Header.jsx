ee39a7c4a83d2975f4fe918f81fd4d445949157c
alexlab84 <alexlab84@hotmail.com>
2024-09-19 19:24:55 +0200

    Add changes in header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,12 @@
 
-
 function Header() {
     return (
       <>
         <h1 className='title'>Generador de refranes divertidos</h1>
         <p className='paragraph'>
-          ¿Sabías que si en cualquier refrán del refranero español pones "en la cama" al final de la primera parte y "entre las piernas" al final de la segunda te queda un refrán mucho más divertido?
+          ¿Sabías que si en cualquier refrán del refranero español pones &quot;en la cama&quot; al final de la
+          primera parte y &quot;entre las piernas&quot; al final de la segunda te queda un refrán mucho más
+          divertido?
         </p>
         <p className='paragraph'>¡Pruébalo tú mismo!</p>
       </>
@@ -13,4 +14,5 @@ function Header() {
   }
   
   export default Header;
+  
   
